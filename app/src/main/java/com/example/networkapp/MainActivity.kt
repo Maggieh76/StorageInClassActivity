fd4d4e0686372d703ace6c6a4c4e5b3469247c97
Margaret Hanley <tuj92824@temple.edu>
2023-11-14 15:20:43 -0500

    working on file saving

--- app/src/main/java/com/example/networkapp/MainActivity.kt
+++ app/src/main/java/com/example/networkapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.networkapp
 
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -14,12 +15,20 @@ import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.squareup.picasso.Picasso
 import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.File
+import java.io.FileOutputStream
+import java.io.FileReader
+import java.io.IOException
 
 // TODO (1: Fix any bugs)
 // TODO (2: Add function saveComic(...) to save and load comic info automatically when app starts)
 
+private const val COMIC_URL_KEY = "comic_url"
+private const val DESCRIPTION_KEY = "description_url"
+private const val TITLE_KEY = "title_comic"
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var preferences: SharedPreferences
     private lateinit var requestQueue: RequestQueue
     lateinit var titleTextView: TextView
     lateinit var descriptionTextView: TextView
@@ -27,10 +36,11 @@ class MainActivity : AppCompatActivity() {
     lateinit var showButton: Button
     lateinit var comicImageView: ImageView
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        preferences = getPreferences(MODE_PRIVATE)
         requestQueue = Volley.newRequestQueue(this)
 
         titleTextView = findViewById<TextView>(R.id.comicTitleTextView)
@@ -38,10 +48,10 @@ class MainActivity : AppCompatActivity() {
         numberEditText = findViewById<EditText>(R.id.comicNumberEditText)
         showButton = findViewById<Button>(R.id.showComicButton)
         comicImageView = findViewById<ImageView>(R.id.comicImageView)
-
         showButton.setOnClickListener {
             downloadComic(numberEditText.text.toString())
         }
+
 
     }
 
@@ -57,6 +67,25 @@ class MainActivity : AppCompatActivity() {
         titleTextView.text = comicObject.getString("title")
         descriptionTextView.text = comicObject.getString("alt")
         Picasso.get().load(comicObject.getString("img")).into(comicImageView)
+        saveComic(
+            comicObject.getString("title"),
+            comicObject.getString("img"),
+            comicObject.getString("alt")
+        )
+
+    }
+    private fun loadComic(){
+        with(preferences){
+            titleTextView.text = getPreferences()
+        }
+    }
+
+    private fun saveComic(comicTitle: String, comicURL: String, comicDescription: String){
+        with(preferences.edit()){
+            putString(TITLE_KEY, comicTitle)
+            putString(COMIC_URL_KEY, comicURL)
+            putString(DESCRIPTION_KEY, comicDescription)
+        }
     }
 
 
